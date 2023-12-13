dbc0761a087f22b05b4fa1556617b514e9c42d62
gitmarge <153720502+gitmarge@users.noreply.github.com>
2023-12-13 19:52:56 +0100

    Create GetActivePowerScheme.ps1

---
+++ Win32/GetActivePowerScheme.ps1
@@ -0,0 +1,34 @@
+# Code example: Access Win32 API function with PowerShell and C#
+#
+# Microsoft Resources
+# https://learn.microsoft.com/en-us/windows/win32/api/powersetting/nf-powersetting-powergetactivescheme
+# https://devblogs.microsoft.com/scripting/use-powershell-to-interact-with-the-windows-api-part-1/
+
+Add-Type @'
+using System;
+using System.Runtime;
+using System.Runtime.InteropServices;
+
+public class ActivePowerScheme
+{
+    [DllImport("PowrProf.dll")]
+    public static extern int PowerGetActiveScheme(ref string UserRootPowerKey, out IntPtr ActivePolicyGuid);
+
+    public struct GuidInfo
+    {
+        public Guid GUID;
+    }
+
+    public static GuidInfo GetGuid()
+    {
+        string urpk = null;
+        IntPtr apg = IntPtr.Zero;
+        int a = PowerGetActiveScheme(ref urpk, out apg);
+        GuidInfo GuidInfoStruct = new GuidInfo();
+        GuidInfoStruct.GUID = Marshal.PtrToStructure<Guid>(apg);
+        return GuidInfoStruct;
+    }
+}
+'@
+
+$ActivePowerSchemeGuid = [ActivePowerScheme]::GetGuid()
